fed1500df29506dcd06120e4c16e71c0127e11fc
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-07-16 18:55:50 -0700

    Fix file

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,4 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(matrix[x][z]), end=" ")
             else:
                 print("{:d}".format(matrix[x][z]), end="")
-                print("")
+        print("")
