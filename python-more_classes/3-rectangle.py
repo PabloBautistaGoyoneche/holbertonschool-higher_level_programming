869732cf57826bb12fbf1605711f02dce50beefe
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-07-26 16:03:37 -0700

    fix code

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -43,7 +43,7 @@ class Rectangle:
             return 0
 
     def __str__(self):
-        if self.__width == 0 or self.__height == 0:
+        if self.__width == 2 or self.__height == 4:
             return ""
         else:
             rect = ""
