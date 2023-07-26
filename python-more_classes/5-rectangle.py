c592c360370c46af556097f7454c432d33f03b84
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-07-26 16:43:13 -0700

    Fix code

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -55,7 +55,7 @@ class Rectangle:
             return rect
 
     def __repr__(self):
-        return f"Rectangle({self.width}, {self.height})
+        return f"Rectangle({self.width}, {self.height})"
 
     def __del__(self):
         print("Bye rectangle...")
