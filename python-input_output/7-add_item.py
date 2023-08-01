d7c6e23b1cc854dcd3797ee43b6db338a11fa07e
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-07-31 18:16:52 -0700

    Updating code

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -17,5 +17,5 @@ if os.path.exists(filename):
     json_obj = load_from_json_file(filename)
 else:
     json_obj = []
-    json_obj.extend(sys.argv[1:])
-    save_to_json_file(json_obj, filename)
+json_obj.extend(sys.argv[1:])
+save_to_json_file(json_obj, filename)
