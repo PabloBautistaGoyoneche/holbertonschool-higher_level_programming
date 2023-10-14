d4e7c711a8d34a8a40dbef765d1c82bd09faeb47
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-10-14 18:58:18 -0500

    Color Me

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the item <header>
+    var headerElement = document.querySelector("header");
+  
+    // Update the text color to red (#FF0000)
+    headerElement.style.color = "#FF0000";
+  });
