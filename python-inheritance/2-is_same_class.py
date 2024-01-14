331f282496947c8c2e8ad8fb3a408d977ef7dc43
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2024-01-14 07:03:38 -0500

    2. Exact same object

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that returns true"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Function that returns True/False if obj is a type of a_class
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
