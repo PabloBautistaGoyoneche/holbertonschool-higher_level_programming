fb7d7b7f75ae56e554dc86fe2392cd33007012a3
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2024-01-19 11:06:32 -0500

    fix code

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,3 @@
--- Script to print the full description of the table from the database
-SHOW TABLES;
+-- Prints the full description of the table first_table from the database
+-- Query to print full description of a table
+SHOW CREATE TABLE first_table;
